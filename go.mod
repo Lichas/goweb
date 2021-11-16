f279da6a4dbde7ce3e2cbc168514c3aa09169a61
luaruan <luaruan@tencent.com>
2021-11-16 22:33:33 +0800

    first sqlite gorm crud app

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module goweb
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.4
+require (
+	github.com/gin-gonic/gin v1.7.4
+	gorm.io/driver/sqlite v1.2.4 // indirect
+	gorm.io/gorm v1.22.3 // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,86 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func NewDB() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("db open err.")
+	}
+	return db
+}
+
+type BlogTag struct {
+	gorm.Model
+	Name string
+}
+
+type Blog struct {
+	gorm.Model
+	Title   string `json:"title"`
+	Content string `json:"content"`
+	TagID   uint
+}
+type Auth struct {
+	gorm.Model
+	Username string
+	Password string
+}
+
+var db *gorm.DB
+
+func initData(db *gorm.DB) {
+	db.AutoMigrate(&BlogTag{})
+	db.AutoMigrate(&Blog{})
+	db.AutoMigrate(&Auth{})
+
+	db.Create(&BlogTag{Name: "mytag"})
+	db.Create(&Blog{Title: "First blog", Content: "my awesome hello text.", TagID: 1})
+	db.Create(&Auth{Username: "Tom", Password: "123456**"})
+
+	var t1 BlogTag
+	db.First(&t1, 1)
+	fmt.Println(t1.Name)
+
+	var b1 Blog
+	db.First(&b1)
+	fmt.Println(b1.Content)
+	var a1 Auth
+	db.First(&a1)
+	fmt.Println(a1.Password)
+}
 
 func main() {
+	db = NewDB()
+	initData(db)
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"pingMessage": "Ping Success.",
-		})
-
-	})
+	r.GET("/ping", Ping) //when c *gin.Context pass in
+	r.GET("/blog", GetBlogs)
 	r.Run()
+}
 
+// Ping ping detect
+func Ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"pingMessage": "Ping Success.",
+	})
+}
+
+// GetBlogs get blogs from db return json
+func GetBlogs(c *gin.Context) {
+	var blogs []Blog
+	if err := db.Find(&blogs).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		c.JSON(200, blogs)
+	}
 }
